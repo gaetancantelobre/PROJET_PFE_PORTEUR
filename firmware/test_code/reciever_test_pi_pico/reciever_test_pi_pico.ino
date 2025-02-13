2af6c821ebc4f83e76644eb3c4a07725be936c3b
Gaetan Cantelobre <gaetancantelobre@gmail.com>
2025-02-13 13:42:53 +0100

    added schmetaic for main board

--- firmware/test_code/reciever_test_pi_pico/reciever_test_pi_pico.ino
+++ firmware/test_code/reciever_test_pi_pico/reciever_test_pi_pico.ino
@@ -5,8 +5,16 @@
 #include <RF24.h>
 #include <Servo.h> // Include the Servo library
 
+
 #define CE_PIN 22 // Adjust if needed
 #define CSN_PIN 17  // Adjust if needed
+
+#include <FastLED.h>
+
+
+#define NUM_LEDS 1    // Number of LEDs in your strip (now 1)
+#define DATA_PIN 23  // Pin connected to the data line of the LED strip
+CRGB leds[NUM_LEDS];
 
 RF24 radio(CE_PIN, CSN_PIN); // CE, CSN
 
@@ -15,7 +23,20 @@ Servo servo; // Create a servo object
 
 const int servoPin = 15; // Define the GPIO pin connected to the servo
 
+
+void rainbow() {
+  static uint8_t hue = 0; // Start hue at 0
+
+  leds[0] = CHSV(hue, 255, 255); // Set the color of the single LED
+
+  hue++; // Increment hue for animation
+}
+
+
 void setup() {
+
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); // Initialize FastLED
+  FastLED.setBrightness(100); // Set brightness (0-255)
   Serial.begin(115200);
 
   delay(1000);
@@ -50,4 +71,7 @@ void loop() {
     }
     servo.write(receivedAngle);
   }
+  rainbow();
+  FastLED.show(); // Update the LEDs
+  delay(10);      // Adjust delay for speed
 }
