c58582c7dbf3538ac3c10292c0a2a6dea72e1761
Joao Fernandes Lopes <61229146+JUSTIKA@users.noreply.github.com>
2025-02-25 18:33:13 +0100

    Functionnal version of the Tello controller
    Adapted version for the Tello Edu controller, made with a command to takeoff and land a few seconds after

--- Tello-controller/tello-edu-py-main/examples/simple-movement.py
+++ Tello-controller/tello-edu-py-main/examples/simple-movement.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
+import sys
+import os
+
 import asyncio
 import tello_edu_protocol as tello
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '/Tello-controller/tello-edu-py-main/tello_edu_protocol/'))
 from tello_edu_protocol.commands import (
     takeoff,
     cw,
@@ -15,10 +19,10 @@ async def main() -> None:
     async with tello.conn(timeout=20) as drone:
         print(f"Battery: {await drone.command(get_battery)}%")
         await drone.command(takeoff)
-        await drone.command(cw, 180)
-        await drone.command(forward, 40)
-        await drone.command(ccw, 180)
-        await drone.command(forward, 40)
+        # await drone.command(cw, 180)
+        # await drone.command(forward, 40)
+        # await drone.command(ccw, 180)
+        # await drone.command(forward, 40)
         await drone.command(land)
 
 
