7a8ee210fe7983d83ee7c3f400f190855a787d54
Gaetan Cantelobre <gaetancantelobre@gmail.com>
2025-02-25 20:48:53 +0100

    [fix] added encoding

--- firmware/nacelle/serial_zero_test.py
+++ firmware/nacelle/serial_zero_test.py
@@ -7,7 +7,7 @@ ser = serial.Serial('/dev/serial0', 9600)  # Default UART on Pi is /dev/serial0 
 time.sleep(2)
 sent_string = input("What message would you like to send to pi zero 2 W ??")
 # Send a serial message
-ser.write(sent_string)
+ser.write(sent_string.encode())
 
 # Close the serial connection when done
 ser.close()
