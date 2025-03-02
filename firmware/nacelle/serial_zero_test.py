e71d8f4057b79f90a159f27f4e0f3ddc6e8ec019
Gaetan Cantelobre <gaetancantelobre@gmail.com>
2025-03-02 13:28:23 +0100

    change baudrate

--- firmware/nacelle/serial_zero_test.py
+++ firmware/nacelle/serial_zero_test.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/serial0', 9600)  # Default UART on Pi is /dev/serial0 (or /dev/ttyAMA0)
+ser = serial.Serial('/dev/serial0', 115200)  # Default UART on Pi is /dev/serial0 (or /dev/ttyAMA0)
 #GPIO 14 (TX) and GPIO 15 (RX)
 # Wait for the connection to establish
 time.sleep(2)
