51c28f9bd2e6802443cda3cb381052cda94faf43
Gaetan Cantelobre <gaetancantelobre@gmail.com>
2025-03-02 13:25:25 +0100

    testing

--- firmware/nacelle/serial_zero_test.py
+++ firmware/nacelle/serial_zero_test.py
@@ -5,10 +5,9 @@ ser = serial.Serial('/dev/serial0', 9600)  # Default UART on Pi is /dev/serial0 
 #GPIO 14 (TX) and GPIO 15 (RX)
 # Wait for the connection to establish
 time.sleep(2)
-while(1):
-    msg = input("msg to send?")
-    # Send a serial message
-    ser.write(msg.encode())
 
-    # Close the serial connection when done
+# Send a serial message
+ser.write(b'Hello, Raspberry Pi!\n')
+
+# Close the serial connection when done
 ser.close()
